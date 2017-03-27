b3cada79bd0dd0df732f0d53031ff96b5e40c450
nughuffer <nughuffer2.0@gmail.com>
2017-03-27 08:21:44 -0400

    Update ArchSetup.sh
    added alias' to zsh config file and uncommented the package installations (oops lol)

--- ArchSetup.sh
+++ ArchSetup.sh
@@ -7,13 +7,30 @@ cd ~/InstallFiles/
 sudo pacman -Syyu
 
 echo "Let's grab some useful programs"
-#sudo pacman -S git curl zsh links
+sudo pacman -S git curl zsh links
 git clone https://github.com/kittykatt/screenFetch/
 sudo cp screenFetch/screenfetch-dev /usr/bin/screenfetch
 sudo rm -R screenFetch
 
 echo "Lets give zsh a makeover!"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+
+cd
+echo "screenfetch -c 3" >> .zshrc
+echo "alias nano="sudo nano" >> .zshrc
+echo "alias update="packer -Syyu" >> .zshrc
+echo "alias aurin="packer -S" >> .zshrc
+echo "alias aurs="packer -Ss" >> .zshrc
+echo "alias remove="sudo pacman -Rdd" >> .zshrc
+echo "alias status="sudo systemctl status" >> .zshrc
+echo "alias enable="sudo systemctl enable" >> .zshrc
+echo "alias disable="sudo systemctl disable" >> .zshrc
+echo "alias restart="sudo systemctl restart" >> .zshrc
+echo "alias failed="sudo systemctl --failed" >> .zshrc
+echo "alias reboot="sudo reboot" >> .zshrc
+echo "alias poweroff="sudo poweroff" >> .zshrc
+echo "alias banner="screenfetch -c 3" >> .zshrc
+
 
 ## Install Packer Dependencies:
 sudo pacman -S expac jshon
