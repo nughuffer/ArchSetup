2134939d8ba48f0120a2426def5b1ec7349cdd18
nughuffer <nughuffer2.0@gmail.com>
2017-12-18 23:16:02 -0500

    Update ArchSetup.sh

--- ArchSetup.sh
+++ ArchSetup.sh
@@ -7,7 +7,7 @@ cd ~/InstallFiles/
 sudo pacman -Syyu
 
 echo "Let's grab some useful programs"
-sudo pacman -S git curl zsh links
+sudo pacman -S git curl zsh links expac jshon
 git clone https://github.com/kittykatt/screenFetch/
 sudo cp screenFetch/screenfetch-dev /usr/bin/screenfetch
 sudo rm -R screenFetch
@@ -15,28 +15,9 @@ sudo rm -R screenFetch
 echo "Lets give zsh a makeover!"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
 
-cd 
-echo '"screenfetch -c 3" 
-echo alias nano="sudo nano"
-echo alias update="packer -Syyu"
-echo alias aurin="packer -S"
-echo alias aurs="packer -Ss"
-echo alias remove="sudo pacman -Rdd"
-echo alias status="sudo systemctl status"
-echo alias enable="sudo systemctl enable"
-echo alias disable="sudo systemctl disable"
-echo alias restart="sudo systemctl restart"
-echo alias failed="sudo systemctl --failed"
-echo alias reboot="sudo reboot" 
-echo alias poweroff="sudo poweroff"
-echo alias banner="screenfetch -c 3"' >> ~/.zshrc
-
-## Install Packer Dependencies:
-sudo pacman -S expac jshon
-
 cd ~/InstallFiles
 
-## Download and Install Packer (this may fail it's a work in progress):
+## Download and Install Packer (this may fail as this is a work in progress):
 mkdir ~/InstallFiles/packer
 cd ~/InstallFiles/packer
 sudo wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer
