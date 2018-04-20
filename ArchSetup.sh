3679cf816dc6917da01cda337ccea58c6d7bce1f
nughuffer <nughuffer2.0@gmail.com>
2018-04-19 20:35:35 -0400

    Update ArchSetup.sh

--- ArchSetup.sh
+++ ArchSetup.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-
-mkdir ~/InstallFiles/
-cd ~/InstallFiles/
-
-# Update the system
+## Update the system
 sudo pacman -Syyu
-
-echo "Let's grab some useful programs"
-sudo pacman -S git curl zsh links expac jshon
+sudo pacman -S curl zsh links expac jshon
 git clone https://github.com/kittykatt/screenFetch/
 sudo cp screenFetch/screenfetch-dev /usr/bin/screenfetch
-sudo rm -R screenFetch
-
-echo "Lets give zsh a makeover!"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
-
-cd ~/InstallFiles
 
 ## Download and Install Packer (this may fail as this is a work in progress):
 mkdir ~/InstallFiles/packer
@@ -25,7 +14,4 @@ mv PKGBUILD?h=packer PKGBUILD
 makepkg
 
 sudo pacman -U packer-*.pkg.tar.xz
-cd
-echo "Cleaning Up Installation..."
-sudo rm -R ~/InstallFiles
 echo "Done!"
