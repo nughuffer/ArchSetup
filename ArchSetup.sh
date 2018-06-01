f0115ce18edf31566a3caffbc0670406107c4c44
nughuffer <nughuffer2.0@gmail.com>
2018-06-01 14:17:00 -0400

    Update ArchSetup.sh
    Packer is a PITA so i'm using yaourt now

--- ArchSetup.sh
+++ ArchSetup.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 ## Update the system
 sudo pacman -Syyu
-sudo pacman -S curl zsh links expac jshon
+sudo pacman -S curl zsh links yaourt
 git clone https://github.com/kittykatt/screenFetch/
 sudo cp screenFetch/screenfetch-dev /usr/bin/screenfetch
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
-
-## Download and Install Packer (this may fail as this is a work in progress):
-mkdir ~/InstallFiles/packer
-cd ~/InstallFiles/packer
-sudo wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer
-mv PKGBUILD?h=packer PKGBUILD
-makepkg
-
-sudo pacman -U packer-*.pkg.tar.xz
-echo "Done!"
