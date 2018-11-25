ed091bbd304590c8ede38383a437285caae9791b
nughuffer <nughuffer2.0@gmail.com>
2018-11-24 21:28:51 -0700

    Update ArchSetup.sh

--- ArchSetup.sh
+++ ArchSetup.sh
@@ -5,7 +5,6 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd
-sudo pacman -S curl zsh links
-git clone https://github.com/kittykatt/screenFetch/
-sudo cp screenFetch/screenfetch-dev /usr/bin/screenfetch
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+sudo pacman -S curl zsh links neofetch
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Done!"
